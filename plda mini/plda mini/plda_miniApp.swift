ba80349deb121bc9ea8c1bac3da616c2117050ad
beanbeen0 <coqlsyc@gmail.com>
2023-11-09 00:52:21 +0900

    백지

---
+++ plda mini/plda mini/plda_miniApp.swift
@@ -0,0 +1,17 @@
+//
+//  plda_miniApp.swift
+//  plda mini
+//
+//  Created by chaebeen on 11/9/23.
+//
+
+import SwiftUI
+
+@main
+struct plda_miniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
